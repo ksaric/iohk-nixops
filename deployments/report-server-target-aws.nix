23ab90945260b16e101ca77726ad87eb7846a589
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-07-03 17:07:08 +0300

    WIP [DEVOPS-188]  Assign *.aws.iohkdev.io to report-server and explorer

--- deployments/report-server-target-aws.nix
+++ deployments/report-server-target-aws.nix
@@ -9,6 +9,9 @@ with (import ./../lib.nix);
       ./../modules/amazon-base.nix
     ];
 
+    deployment.route53.accessKeyId = accessKeyId;
+    deployment.route53.hostName = "report-server.aws.iohkdev.io";
+
     deployment.ec2.accessKeyId = accessKeyId;
   };
 }
