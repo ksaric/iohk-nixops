cc61af71f164279c6ce426348924719b4e2bc97c
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-07-05 17:40:34 +0300

    [DEVOPS-56]  Tie them all: interpret `cluster.nix`'s connectivity into `staticPeers` and `initialKademliaPeers`

--- modules/cardano-node-config.nix
+++ modules/cardano-node-config.nix
@@ -2,6 +2,10 @@
 # XXX: rename this file:  cardano-node-config vs. cardano-nodes-config is CRUEL
 with (import ./../lib.nix);
 
+let
+    nodeIdToAddress = i: "node${toString i}";  # XXX: this is a stub
+in
+
 testIndex: region: connectivity:
   { pkgs, ...}: {
     imports = [
@@ -14,5 +18,16 @@ testIndex: region: connectivity:
       port = cconf.nodePort;
       inherit (cconf) enableP2P genesisN slotDuration networkDiameter mpcRelayInterval;
       inherit (cconf) totalMoneyAmount bitcoinOverFlat productionMode systemStart richPoorDistr;
-    };
+    }   //
+     (      if connectivity.type == "core"       then with connectivity; {
+        staticPeers          = map nodeIdToAddress (corePeers ++ coreRelayPeers);
+        initialKademliaPeers = [];
+     } else if connectivity.type == "core-relay" then with connectivity; {
+        staticPeers          = map nodeIdToAddress (corePeers);
+        initialKademliaPeers = map nodeIdToAddress (coreRelayPeers ++ pureRelayPeers);
+     } else if connectivity.type == "pure-relay" then with connectivity; {
+        staticPeers          = [];
+        initialKademliaPeers = map nodeIdToAddress (coreRelayPeers ++ pureRelayPeers);
+     } else
+        throw "Invalid connectivity.type '${connectivity.type}' for node ${toString testIndex}, must be one of: 'core', 'core-relay' and 'pure-relay'.");
   }
