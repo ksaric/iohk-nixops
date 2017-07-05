46affa8d6ab05425a248fdfe22d4ec7b792550b4
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-07-05 23:35:09 +0300

    [DEVOPS-56]  Resolve node ids to public IP addresses.

--- modules/cardano-node-config.nix
+++ modules/cardano-node-config.nix
@@ -2,17 +2,23 @@
 # XXX: rename this file:  cardano-node-config vs. cardano-nodes-config is CRUEL
 with (import ./../lib.nix);
 
-let
-    nodeIdToAddress = i: "node${toString i}";  # XXX: this is a stub
-in
-
 testIndex: region: connectivity:
-  { pkgs, ...}: {
+  { pkgs, nodes, ...}: {
     imports = [
       ./common.nix
     ];
 
-    services.cardano-node = {
+    services.cardano-node =
+    let
+      nodeById = i: let xs = filter (n: n.config.services.cardano-node.testIndex == i) (attrValues nodes);
+                    in if xs != [] then builtins.elemAt xs 0
+                       else throw "nodeById: no node with ID ${toString i}.";
+      nodePublicIP = n: let ip = n.config.services.cardano-node.publicIP;
+                        in if ip != null then ip
+                           else throw "nodePublicIP: node #${toString n.config.services.cardano-node.testIndex} has no public IP configured.";
+      nodeIdToPublicIP = i: let ip = nodePublicIP (nodeById i);
+                            in builtins.trace "resolved node id #${toString i} -> ${ip}" ip;
+    in {
       enable = true;
       testIndex = testIndex;
       port = cconf.nodePort;
@@ -20,14 +26,14 @@ testIndex: region: connectivity:
       inherit (cconf) totalMoneyAmount bitcoinOverFlat productionMode systemStart richPoorDistr;
     }   //
      (      if connectivity.type == "core"       then with connectivity; {
-        staticPeers          = map nodeIdToAddress (corePeers ++ coreRelayPeers);
+        staticPeers          = map nodeIdToPublicIP (corePeers ++ coreRelayPeers);
         initialKademliaPeers = [];
      } else if connectivity.type == "core-relay" then with connectivity; {
-        staticPeers          = map nodeIdToAddress (corePeers);
-        initialKademliaPeers = map nodeIdToAddress (coreRelayPeers ++ pureRelayPeers);
+        staticPeers          = map nodeIdToPublicIP (corePeers);
+        initialKademliaPeers = map nodeIdToPublicIP (coreRelayPeers ++ pureRelayPeers);
      } else if connectivity.type == "pure-relay" then with connectivity; {
         staticPeers          = [];
-        initialKademliaPeers = map nodeIdToAddress (coreRelayPeers ++ pureRelayPeers);
+        initialKademliaPeers = map nodeIdToPublicIP (coreRelayPeers ++ pureRelayPeers);
      } else
         throw "Invalid connectivity.type '${connectivity.type}' for node ${toString testIndex}, must be one of: 'core', 'core-relay' and 'pure-relay'.");
   }
